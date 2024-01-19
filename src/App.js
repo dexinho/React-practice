ef0f61aaeb19f19cd38fb1cfb82569008c5de426
dexinho <dexinho008@gmail.com>
2024-01-19 17:29:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import DisplayImages from "./DisplayImages/DisplayImages";
 
-function App() {
+const App = () => {
+  const images = [
+    {
+      title: "Atomic Habits",
+      writer: "James Clear",
+      imageURL: "./assets/1.jpg",
+    },
+    {
+      title: "Fourth Wing",
+      writer: "Rebecca Yarros",
+      imageURL: "../src/assets/2.jpg",
+    },
+    {
+      title: "House of Flame and Shadow",
+      writer: "Sarah J. Maas",
+      imageURL: "../src/assets/3.jpg",
+    },
+    {
+      title: "The Bible in 52 Weeks",
+      writer: "Dr. Kimerly D. Moore",
+      imageURL: "../src/assets/4.jpg",
+    },
+    {
+      title: "Iron Flame",
+      writer: "Rebecca Yarros",
+      imageURL: "../src/assets/5.jpg",
+    },
+    {
+      title: "The Kingdom",
+      writer: "Tim Alberta",
+      imageURL: "../src/assets/6.jpg",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {images.map((image, index) => (
+        <DisplayImages
+          key={index}
+          bookTitle={image.title}
+          bookWriter={image.writer}
+          bookImage={image.imageURL}
+        />
+      ))}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Components/pozdrav.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Pozdrav = () => {
+
+  return (<h1 className="zizo">Zdravo</h1>);
+};
+
+export default Pozdrav
